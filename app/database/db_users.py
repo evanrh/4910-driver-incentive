96e362a796233e0864b3e29fa45e561ef9258a92
Will Sherrer <wsherre@clemson.edu>
2020-10-17 18:41:02 -0400

    view apps and drivers also return driver id

--- app/database/db_users.py
+++ app/database/db_users.py
@@ -574,7 +574,7 @@ class Sponsor(AbsUser):
             raise Exception(e)
 
     def view_applications(self):
-        query = 'SELECT driver.user FROM driver INNER JOIN driver_bridge ON driver.driver_id = driver_bridge.driver_id WHERE driver_bridge.sponsor_id = %s AND apply = 1'
+        query = 'SELECT driver.user driver.driver_id FROM driver INNER JOIN driver_bridge ON driver.driver_id = driver_bridge.driver_id WHERE driver_bridge.sponsor_id = %s AND apply = 1'
         vals = (self.properties['id'])
 
         try: 
@@ -586,7 +586,7 @@ class Sponsor(AbsUser):
 
 
     def view_drivers(self):
-        query = 'SELECT driver.user, driver_bridge.points FROM driver INNER JOIN driver_bridge ON driver.driver_id = driver_bridge.driver_id WHERE driver_bridge.sponsor_id = %s AND apply = 0 ORDER BY driver_bridge.points DESC'
+        query = 'SELECT driver.user, driver.driver_id, driver_bridge.points FROM driver INNER JOIN driver_bridge ON driver.driver_id = driver_bridge.driver_id WHERE driver_bridge.sponsor_id = %s AND apply = 0 ORDER BY driver_bridge.points DESC'
         vals = (self.properties['id'])
 
         try: 

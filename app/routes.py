d70a9785a82739ad6a3f6b470b8efe0468b00098
Evan Hastings <erhasti@clemson.edu>
2020-10-19 16:33:17 -0400

    Fixed templating issue in sponsor views

--- app/routes.py
+++ app/routes.py
@@ -477,17 +477,17 @@ def updateDriver(username):
     global database
     dl = Driver().get_users()
     driver = list(filter(lambda d: d[3] == username, dl))[0]
+    driverObj = Driver()
+    driverObj.populate(username)
     if request.method == 'POST':
         data = request.json
         if 'addPoints' in data.keys():
             add_points_to_driver(username, 0, data['addPoints'])
             return json.dumps({'status': 'OK', 'ptsAdded': data['addPoints']})
-        driverObj = Driver()
-        driverObj.populate(username)
 
         # Data should be formatted in the way update_info expects
         driverObj.update_info(data)
         flash("Information updated!")
         return json.dumps({'status': 'OK', 'user': username})
 
-    return render_template("sponsor/sponsorEditDriver.html", driver=driver)
+    return render_template("sponsor/sponsorEditDriver.html", driver=driverObj)

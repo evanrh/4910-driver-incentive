59592e0e4ccaabe4560807f3dcb6bb2975164033
Evan Hastings <erhasti@clemson.edu>
2020-10-25 01:53:30 -0400

    Added better token functionality to sponsor api routes

--- app/api_routes.py
+++ app/api_routes.py
@@ -4,7 +4,7 @@ from flask import request
 from app import api
 from app import app
 from app.products.etsy_driver import EtsyController
-from app.database import *
+from app.database.db_users import *
 from itsdangerous import (TimedJSONWebSignatureSerializer
                             as Serializer, BadSignature, SignatureExpired)
 from functools import wraps
@@ -12,9 +12,27 @@ import os
 
 conn = EtsyController(os.getenv('ETSY_API_KEY'))
 
+def generate_auth_token(sponsor_username, expiration = 3600):
+    s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+    db = getConnection()
+    query = "SELECT sponsor_id FROM sponsor WHERE user=%s"
+    results = db.query(query, (sponsor_username, ))
+    if results:
+        return s.dumps({'id': results[0][0]})
+    else:
+        return None
+
 def verify_auth_token(token):
     # Fetch token from database
-    pass
+    s = Serializer(app.config['SECRET_KEY'])
+
+    try:
+        data = s.loads(token)
+    except SignatureExpired:
+        return False
+    except BadSignature:
+        return False
+    return True
 
 # Decorator to do token based auth
 def token_required(f):
@@ -22,18 +40,16 @@ def token_required(f):
     def decorated(*args, **kwargs):
         if 'X-API-KEY' in request.headers:
             token = request.headers['X-API-KEY']
-        if not token:
-            return {'message': 'Token is missing'}
+            if not token:
+                return {'message': 'Token is missing'}
 
-        if token != 'mytoken':
-            return {'message': 'Your token is wrong'}
-        print('TOKEN: {}'.format(token))
-        return f(*args, **kwargs)
+            if not verify_auth_token(token):
+                return {'message': 'Your token is bad. Generate a new one.'}
+            print('TOKEN: {}'.format(token))
+            return f(*args, **kwargs)
+        else:
+            return {'message': 'Token is missing'}
     return decorated
-@api.route('/hello')
-class Hello(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
 catalog_item = api.model('Catalog_Item', {
     'title': fields.String,
@@ -51,14 +67,19 @@ class SponsorCatalog(Resource):
     def get(self):
         return {'pid': 1}
 
+    @token_required
+    @api.doc(security='apikey')
     @api.expect(catalog_item)
     def post(self):
         item = api.payload
         print(item)
         return {'data': 'Item added'}, 200
         
-@api.route('/sponsor/api/auth')
+@api.route('/sponsor/api/auth/<string:sponsor_username>')
 class SponsorAPIAuth(Resource):
-    def get(self):
-        token = generate_auth_token()
-        return {'token': token.decode('ascii')}, 200
+    def get(self, sponsor_username):
+        token = generate_auth_token(sponsor_username)
+        if token:
+            return {'token': token.decode('ascii')}, 200
+        else:
+            return {'message': 'Sponsor name not found'}, 400
